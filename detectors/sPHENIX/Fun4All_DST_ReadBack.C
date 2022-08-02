0505d31eae7365d95a0bba07be3ad310bab427cc
Jin Huang <jhuang@bnl.gov>
2022-08-02 00:09:02 -0400

    add readback-plot

--- detectors/sPHENIX/Fun4All_DST_ReadBack.C
+++ detectors/sPHENIX/Fun4All_DST_ReadBack.C
@@ -13,18 +13,37 @@
 #include <fun4all/Fun4AllServer.h>
 #include <fun4all/SubsysReco.h>
 
+#include <g4eval/PHG4DSTReader.h>
 #include <TSystem.h>
+#include <phool/recoConsts.h>
+
 
 R__LOAD_LIBRARY(libfun4all.so)
+R__LOAD_LIBRARY(libg4eval.so)
 
-void Fun4All_DST_ReadBack(const int nevnt = 0, const char *inputfile = "G4sPHENIX.root")
+// void Fun4All_DST_ReadBack(const int nevnt = 100, const char *inputfile = "data/MDC2_Run41_FTFP_BERT_HP.g4hits.list")
+void Fun4All_DST_ReadBack(const int nevnt = 100, const char *inputfile = "data/MDC2_Run40_FTFP_BERT.g4hits.list")
 {
   gSystem->Load("libg4dst");
   Fun4AllServer *se = Fun4AllServer::instance();
+  se->Verbosity(1);
+//  recoConsts *rc = recoConsts::instance();
+//  rc->set_IntFlag("RUNNUMBER", 40);
 
   Fun4AllInputManager *in = new Fun4AllDstInputManager("DSTin");
-  in->fileopen(inputfile);
+  in->AddListFile(inputfile);
   se->registerInputManager(in);
+
+
+
+  // save a comprehensive  evaluation file
+  PHG4DSTReader *ana = new PHG4DSTReader(string(inputfile) + ".DSTReader.root");
+  ana->set_save_particle(true);
+  ana->set_load_all_particle(true);
+  ana->set_load_active_particle(true);
+  ana->set_save_vertex(true);
+  ana->AddNode("TPC");
+  se->registerSubsystem(ana);
 
   se->run(nevnt);
   se->End();
